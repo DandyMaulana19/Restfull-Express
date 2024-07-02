cbeecd64b09f970bfe61f933e41c30b666927f6e
Dandymay <dandyainul19@gmail.com>
2024-07-02 11:36:26 +0700

    feat: adding dummy data & create endpoint

--- index.js
+++ index.js
@@ -8,8 +8,33 @@ const port = process.env.PORT;
 //   console.log("test");
 // });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const datas = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 28 },
+  { name: "Bob", age: 35 },
+  { name: "Alice", age: 27 },
+  { name: "David", age: 32 },
+  { name: "Eve", age: 31 },
+  { name: "Frank", age: 33 },
+  { name: "Grace", age: 29 },
+  { name: "Henry", age: 34 },
+  { name: "Isabella", age: 36 },
+  { name: "Jack", age: 37 },
+  { name: "Karen", age: 38 },
+  { name: "Lisa", age: 39 },
+];
+
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send({ datas });
+});
+
+app.post("/", (req, res) => {
+  const { name, age } = req.body;
+  datas.push({ name, age });
+  res.redirect("/");
 });
 
 app.get("/${hallo}", (req, res) => {
